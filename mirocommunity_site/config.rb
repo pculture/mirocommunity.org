87061328d3dbc2f2620e5cff94d421645d55ef05
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-04-19 20:32:51 -0700

    Initial move from the new css to sass.

---
+++ mirocommunity_site/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/mirocommunity_site/css"
+sass_dir = "sass"
+images_dir = "static/mirocommunity_site/images"
+javascripts_dir = "static/mirocommunity_site/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
