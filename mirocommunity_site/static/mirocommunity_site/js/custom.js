9767707d0cc21c3d2c1270bd1b22b4bc88985e9d
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-04-27 22:49:27 -0700

    Removed the css hack js.

--- mirocommunity_site/static/mirocommunity_site/js/custom.js
+++ mirocommunity_site/static/mirocommunity_site/js/custom.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function(){
-	
-	$('.footer-nav li:last-child').css("background","none");
-	$('#features ul li:last-child').css('margin-right','0px');
-	$('.footer-nav li:first-child a').css('padding-left','0px');
+
 	//this function attached focus and blur events with input elements
 	var addFocusAndBlur = function($input, $val){
 		
