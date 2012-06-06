f9663c92f8b9d2177db98473e58772ad3cf78dd9
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-06-06 15:34:23 -0700

    Added base non-secret settings/requirements files for mc.org.

---
+++ mirocommunity_site/base_settings.py
@@ -0,0 +1,65 @@
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+	("Miro Community Devs", "dev@mirocommunity.org"),
+)
+
+MANAGERS = (
+	("Miro Community Support", "support@mirocommunity.org"),
+)
+
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
+USE_L10N = True
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mirocommunity_site',
+)
+
+ROOT_URLCONF = 'mirocommunity_site.urls'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+)
+
+# The following settings are needed per-environment:
+# - SECRET_KEY
+# - MEDIA_ROOT
+# - STATIC_ROOT
+# - DATABASES
+# - logging/tracing settings (if applicable)
+# - PROJECT_ROOT
+# - PROJECT_SCRIPT
+# - PROJECT_REDIRECT_SCRIPT
